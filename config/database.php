ccc357d21fe879a2fe406a7d0deeac3297b1d9cc
fai <fai_@mail.ru>
2020-03-16 21:52:54 +0400

    fix second connect

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'h002294501_old'),
+            'database' => env('DB_DATABASE_OLD', 'h002294501_old'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8mb4',
