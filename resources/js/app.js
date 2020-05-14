94b917586fba4abeb95e61a3f355a5004d8c6b4c
Fukalov <andrey.fukalov@payoola.com>
2020-05-15 00:04:59 +0400

    build band prod

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,53 +1,12 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-import axios from 'axios';
-
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-// import { yandexMap, ymapMarker } from 'vue-yandex-maps'
-
-// import YmapPlugin from 'vue-yandex-maps'
-
-// Vue.use(YmapPlugin, settings);
-
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('pay-service', require('./components/PayService.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-// const settings = {
-//     apiKey: 'e5db01fb-1c20-456d-b884-cf0d71279a63',
-//     lang: 'ru_RU',
-//     coordorder: 'latlong',
-//     version: '2.1'
-// }
 
 
 const app = new Vue({
     el: '#app',
-    // components: { yandexMap, ymapMarker },
     data() {
         return {
             password: '',
@@ -80,33 +39,6 @@ const app = new Vue({
 
 
 $(document).ready(function() {
-
-    /*
-            $.ajax({
-                url: "/vso.php",
-                data: {},
-                dataType: "html",
-                    success: function(data){
-    //				alert(data);
-                    $('#flash').html(data);
-                },
-                    error: function(data){
-                    $('#flash').html('<h1>Ошибка показа</h1>');
-                },
-                    complete: function(data){
-                }
-            });
-
-    */
-
-    // if($("#password").is("selector")) {
-    //     console.log('show news password');
-    //     $("#password").password('toggle');
-    // }
-    // else {
-    //     console.log('not found password');
-    // }
-
     $(document).on("click", "#video1-play", function () {
 
         var  video = document.getElementById("video1");
